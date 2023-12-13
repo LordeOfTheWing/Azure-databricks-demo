052bdc4f99c086e764886d7ad041d0aa92597978
LordeOfTheWing <LordeOfTheWing>
2023-12-13 15:45:25 +0000

    Add notebook configuration file

---
+++ formula1_project/Includes/configuration.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+BRONZE_LAYER_PATH = "abfss://bronze@saprojectmaestrosnd.dfs.core.windows.net/formula1_raw"
+SILVER_LAYER_PATH = "abfss://silver@saprojectmaestrosnd.dfs.core.windows.net/processed"
+GOLD_LAYER_PATH = "abfss://gold@saprojectmaestrosnd.dfs.core.windows.net/presentation"
